8243d25f1bbd5dd6305b6faf81a6854963109e1e
e-ManueI <19648538+e-ManueI@users.noreply.github.com>
2023-04-01 14:34:47 +0300

    resolved migrations

--- GigGo/settings.py
+++ GigGo/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'GigGo_App',
 ]
 
 MIDDLEWARE = [
@@ -79,6 +81,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+AUTH_USER_MODEL = 'GigGo_App.NewUser'
 
 
 # Password validation
